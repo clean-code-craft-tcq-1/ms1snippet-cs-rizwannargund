a98be42f259d4323c580f737c2c7abdd696f7b64
rizwannargund <58382619+rizwannargund@users.noreply.github.com>
2021-03-23 20:13:22 +0530

    Limit complexity changes

--- SensorValidate.Tests/SensorValidatorTest.cs
+++ SensorValidate.Tests/SensorValidatorTest.cs
@@ -8,13 +8,13 @@ namespace SensorValidate.Tests
     public class SensorValidatorTest
     {
         [Fact]
-        public void reportsErrorWhenSOCjumps() {
+        public void ReportsErrorWhenSOCJumps() {
             Assert.False(SensorValidator.ValidateSOCReadings(
                 new List<double>{0.0, 0.01, 0.5, 0.51}
             ));
         }
         [Fact]
-        public void reportsErrorWhenCurrentjumps() {
+        public void ReportsErrorWhenCurrentJumps() {
             Assert.False(SensorValidator.ValidateCurrentReadings(
                 new List<double>{0.03, 0.03, 0.03, 0.33}
             ));
@@ -27,7 +27,7 @@ namespace SensorValidate.Tests
             ));
         }
         [Fact]
-        public void ErrorWhenCurrentReadingsHasNoValues()
+        public void WhenCurrentReadingsHasNoValues()
         {
             Assert.Throws<ArgumentNullException>(() => SensorValidator.ValidateCurrentReadings(
                 null
--- SensorValidate/SensorValidate.cs
+++ SensorValidate/SensorValidate.cs
@@ -6,12 +6,10 @@ namespace SensorValidate
     public class SensorValidator
     {
         private static bool IsValueSuddenJumped(double value, double maxDelta) => !(value > maxDelta);
-        public static bool ValidateSOCReadings(List<Double> values) => IsAnyValueSuddenJumped(values, 0.05);
-        public static bool ValidateCurrentReadings(List<Double> values) => IsAnyValueSuddenJumped(values, 0.1);
+        public static bool ValidateSOCReadings(List<Double> values) => IsAnyValueSuddenJumped(values ?? throw new ArgumentNullException("values cannot be null."), 0.05);
+        public static bool ValidateCurrentReadings(List<Double> values) => IsAnyValueSuddenJumped(values ?? throw new ArgumentNullException("values cannot be null."), 0.1);
         private static bool IsAnyValueSuddenJumped(List<Double> values, double maxDelta)
         {
-            if (values == null)
-                throw new ArgumentNullException("values cannot be null.");
             for (int i = 0; i < (values?.Count - 1); i++)
             {
                 if (!IsValueSuddenJumped(values[i + 1] - values[i], maxDelta))
