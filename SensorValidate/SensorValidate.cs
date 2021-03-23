b7aa2b4c6ca13ae899b4dccfd54f3701c9ebfc59
rizwannargund <58382619+rizwannargund@users.noreply.github.com>
2021-03-23 20:50:45 +0530

    limit complexity for null error throw

--- SensorValidate/SensorValidate.cs
+++ SensorValidate/SensorValidate.cs
@@ -6,18 +6,18 @@ namespace SensorValidate
     public class SensorValidator
     {
         private static bool IsValueSuddenJumped(double value, double maxDelta) => !(value > maxDelta);
-        public static bool ValidateSOCReadings(List<Double> values) => IsAnyValueSuddenJumped(values ?? throw new ArgumentNullException("values cannot be null."), 0.05);
-        public static bool ValidateCurrentReadings(List<Double> values) => IsAnyValueSuddenJumped(values ?? throw new ArgumentNullException("values cannot be null."), 0.1);
+        public static bool ValidateSOCReadings(List<Double> values) => IsAnyValueSuddenJumped(NullCheck(values), 0.05);
+        public static bool ValidateCurrentReadings(List<Double> values) => IsAnyValueSuddenJumped(NullCheck(values), 0.1);
         private static bool IsAnyValueSuddenJumped(List<Double> values, double maxDelta)
         {
             for (int i = 0; i < (values?.Count - 1); i++)
             {
                 if (!IsValueSuddenJumped(values[i + 1] - values[i], maxDelta))
-                {
                     return false;
-                }
             }
             return true;
         }
+        private static List<Double> NullCheck(List<Double> values) => (values == null ? throw new ArgumentNullException("values cannot be null.") : values);
+
     }
 }
