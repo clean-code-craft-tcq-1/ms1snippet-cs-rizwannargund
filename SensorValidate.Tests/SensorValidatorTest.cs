71d4ce88c977c3890a7847f0449759b5d1b9aa8d
rizwannargund <58382619+rizwannargund@users.noreply.github.com>
2021-03-23 20:02:55 +0530

    First commit for SensorValidate

--- SensorValidate.Tests/SensorValidatorTest.cs
+++ SensorValidate.Tests/SensorValidatorTest.cs
@@ -9,14 +9,28 @@ namespace SensorValidate.Tests
     {
         [Fact]
         public void reportsErrorWhenSOCjumps() {
-            Assert.False(SensorValidator.validateSOCreadings(
+            Assert.False(SensorValidator.ValidateSOCReadings(
                 new List<double>{0.0, 0.01, 0.5, 0.51}
             ));
         }
         [Fact]
         public void reportsErrorWhenCurrentjumps() {
-            Assert.False(SensorValidator.validateCurrentreadings(
+            Assert.False(SensorValidator.ValidateCurrentReadings(
                 new List<double>{0.03, 0.03, 0.03, 0.33}
+            ));
+        }
+        [Fact]
+        public void WhenSOCReadingsHasNoValues()
+        {
+            Assert.Throws<ArgumentNullException>(() => SensorValidator.ValidateSOCReadings(
+                null
+            ));
+        }
+        [Fact]
+        public void ErrorWhenCurrentReadingsHasNoValues()
+        {
+            Assert.Throws<ArgumentNullException>(() => SensorValidator.ValidateCurrentReadings(
+                null
             ));
         }
     }
